222926d518285204990ce8a4204bdbcff153dcb2
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-04-05 14:30:33 +0200

    Added Gradle project

---
+++ app/src/main/java/cookbook/App.java
@@ -0,0 +1,36 @@
+package cookbook;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox root = new VBox();
+        root.setPadding(new Insets(5));
+        Label title = new Label("JavaFX");
+        Label mysql;
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/StarWars?user=tobias&password=abcd1234&useSSL=false");
+            mysql = new Label("Driver found and connected");
+
+        } catch (SQLException e) {
+            mysql = new Label("An error has occurred: " + e.getMessage());
+        }
+
+        root.getChildren().addAll(title, mysql);
+
+        primaryStage.setScene(new Scene(root, 400, 200));
+        primaryStage.setTitle("JavaFX");
+        primaryStage.show();
+    }
+}
