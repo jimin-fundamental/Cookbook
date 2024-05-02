5660169f6d28720fe61f52df70af605f61a6ec8c
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-05-02 12:21:34 +0200

    Added way of viewing the Weekly Dinner List mock

---
+++ app/src/main/java/cookbook/Controller/WeeklyListSceneController.java
@@ -0,0 +1,13 @@
+package cookbook.Controller;
+
+import cookbook.model.Recipe;
+import javafx.fxml.FXML;
+
+public class WeeklyListSceneController {
+    private Recipe recipe;
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+}
