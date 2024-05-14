cc0962e436a0194d69bf437928af75a4d3d67c3e
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-05-15 01:22:12 +0200

    Added Edit functionality

--- app/src/main/java/cookbook/Controller/ModifyUserSceneController.java
+++ app/src/main/java/cookbook/Controller/ModifyUserSceneController.java
@@ -1,6 +1,8 @@
 package cookbook.Controller;
 
+import cookbook.DatabaseManager;
 import cookbook.model.User;
+import cookbook.repository.UserDao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -33,6 +35,24 @@ public class ModifyUserSceneController {
         usernameField.setText(user.getUserName());
         passwordField.setText(user.getPassword());
         isAdminCheckBox.setSelected(user.getIsAdmin() == 1);
+    }
+
+    @FXML
+    public void saveClicked(ActionEvent event) {
+        user.setName(nameField.getText());
+        user.setUserName(usernameField.getText());
+        user.setPassword(passwordField.getText());
+        user.setIsAdmin(isAdminCheckBox.isSelected() ? 1 : 0);
+
+        DatabaseManager dbManager = new DatabaseManager();
+        UserDao userDao = new UserDao(dbManager);
+
+        // save user
+        userDao.editUser(user);
+
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 
     @FXML
