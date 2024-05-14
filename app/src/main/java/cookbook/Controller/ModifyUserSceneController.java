d81ff2df4ebeca0416adb3b69024e5fa37fa3d88
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-05-15 01:15:57 +0200

    Added information on fields of Modify User

--- app/src/main/java/cookbook/Controller/ModifyUserSceneController.java
+++ app/src/main/java/cookbook/Controller/ModifyUserSceneController.java
@@ -4,6 +4,8 @@ import cookbook.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ModifyUserSceneController {
@@ -13,8 +15,24 @@ public class ModifyUserSceneController {
     @FXML
     Button closeButton;
 
+    @FXML
+    TextField nameField;
+
+    @FXML
+    TextField usernameField;
+
+    @FXML
+    TextField passwordField;
+
+    @FXML
+    CheckBox isAdminCheckBox;
+
     public void setUser(User user) {
         this.user = user;
+        nameField.setText(user.getName());
+        usernameField.setText(user.getUserName());
+        passwordField.setText(user.getPassword());
+        isAdminCheckBox.setSelected(user.getIsAdmin() == 1);
     }
 
     @FXML
