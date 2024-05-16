72523936d50799de11f5b35035ef37d3f1b83d83
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-05-16 18:23:59 +0200

    Added help items render on HelpView

--- app/src/main/java/cookbook/Controller/HelpViewSceneController.java
+++ app/src/main/java/cookbook/Controller/HelpViewSceneController.java
@@ -1,11 +1,22 @@
 package cookbook.Controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import cookbook.DatabaseManager;
+import cookbook.model.Help;
+import cookbook.model.Recipe;
+import cookbook.repository.HelpDao;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 
 public class HelpViewSceneController {
+
+    private List<Help> helpEntries;
 
     @FXML
     private VBox helpListVBox;
@@ -18,8 +29,31 @@ public class HelpViewSceneController {
 
     }
 
+    private void retrieveHelpEntries() {
+        DatabaseManager dbManager = new DatabaseManager();
+        HelpDao helpDao = new HelpDao(dbManager);
+        helpEntries = helpDao.getAllHelp();
+    }
+
+    private void displayHelpItem(Help helpEntry) {
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/cookbook.view/HelpItem.fxml"));
+            VBox helpBox = fxmlLoader.load();
+            HelpItemController controller = fxmlLoader.getController();
+            controller.setHelpItem(helpEntry);
+            helpListVBox.getChildren().add(helpBox);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void initialize(){
-        
+        retrieveHelpEntries();
+        for (Help help : helpEntries) {
+            displayHelpItem(help);
+        }
     }
 }
 
