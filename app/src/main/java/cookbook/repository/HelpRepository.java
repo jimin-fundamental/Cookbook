bcc29d40de7f6c91acb21b38fb14ccd350e5ec7d
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-05-16 18:15:59 +0200

    Added fetch help from database

---
+++ app/src/main/java/cookbook/repository/HelpRepository.java
@@ -0,0 +1,11 @@
+package cookbook.repository;
+
+import java.util.List;
+
+import cookbook.model.Help;
+
+public interface HelpRepository {
+    List<Help> getAllHelp();
+    // Other user-related methods
+}
+
