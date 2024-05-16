8a09899536ee4aede584c89493b106802825438c
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2024-05-16 17:54:59 +0200

    Added getters and setters to the HelpScene texts

--- app/src/main/java/cookbook/Controller/HelpSceneController.java
+++ app/src/main/java/cookbook/Controller/HelpSceneController.java
@@ -1,8 +1,24 @@
 package cookbook.Controller;
 
+import cookbook.model.Help;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class HelpSceneController {
+
+    @FXML
+    private VBox helpVBox;
+
+    @FXML
+    private Text helpTitle;
+
+    private Help helpItem;
+
+    public void setHelpItem(Help item) {
+        helpItem = item;
+        helpTitle.setText(item.getTitle());
+    }
 
     @FXML
     public void BackClicked() {
